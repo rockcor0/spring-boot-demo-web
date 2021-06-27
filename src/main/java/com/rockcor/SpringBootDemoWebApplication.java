fc69ecbc824e5b2f2b585baf31e35c136d31dd8d
Ricardo Delgado <ridel007@gmail.com>
2021-06-26 19:25:24 -0500

    first

---
+++ src/main/java/com/rockcor/SpringBootDemoWebApplication.java
@@ -0,0 +1,13 @@
+package com.rockcor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoWebApplication.class, args);
+	}
+
+}
